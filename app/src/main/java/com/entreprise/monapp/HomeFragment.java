b31020f5f96eeaeaaed331ec127d35508157568f
maxime esprit <maxime.esprit@gmail.com>
2020-06-29 12:29:41 +0200

    Appel web service réussi et affichage dans un Toast

--- app/src/main/java/com/entreprise/monapp/HomeFragment.java
+++ app/src/main/java/com/entreprise/monapp/HomeFragment.java
@@ -10,6 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 /**
@@ -36,7 +46,6 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-
         welcomeTxv = v.findViewById(R.id.welcomeTextView);
         mBtnShowDetailActivity = v.findViewById(R.id.fragmentHome_Show_DetailActivity_Button);
 
@@ -54,6 +63,41 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        callWebService();
+
         return v;
+    }
+
+    private void callWebService() {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        // Build URL
+        String urlBase = getResources().getString(R.string.URL_Webservice_Base);
+        String url = urlBase + "lyon";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //Affiche un texte temporaire à l'écran, qui disparait après quelques secondes.
+                        Toast.makeText(getActivity(), response, Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(getActivity(), "ERROR : " + error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
